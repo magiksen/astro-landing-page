7d33c859533d13c8e0379d3e6e260d8f4e94d8a5
magiksen <elemejore@gmail.com>
2023-01-03 20:20:35 -0400

    Vercel deploy done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://magiksen.github.io',
-  base: '/astro-landing-page',
+  output: "server",
+  adapter: vercel()
 });
